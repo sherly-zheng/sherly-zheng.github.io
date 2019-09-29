c1fa17610adcc3320ad6ad136aa41afedcf4558e
Sherly Zheng <zheng.sherly@gmail.com>
2019-09-28 22:05:43 -0400

    Updated contact buttons

---
+++ scripts/jquery.copy.js
@@ -0,0 +1,12 @@
+function copy(btn){
+	$(btn).siblings(".contact__input").select();
+	document.execCommand("copy");
+	$(".far", btn).removeClass("far").addClass("fas");
+}
+
+$(document).ready(function(){
+	$(".contact__container").mouseout(function(){
+		$(".contact__copy .fas").removeClass("fas").addClass("far");
+		document.getSelection().removeAllRanges();
+	});
+})
